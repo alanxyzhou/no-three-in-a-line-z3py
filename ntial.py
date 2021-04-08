c168334ad0df96695a40b1bfa6712032507821a2
alanxyzhou <30447373+alanxyzhou@users.noreply.github.com>
2021-04-08 00:29:42 -0400

    attempt sums of distances

--- ntial.py
+++ ntial.py
@@ -161,17 +161,16 @@ def three_solve(N):
 
     # strategy:
     # add 2n points
-    # for each point, constraint is that it must form a unique line equation with every other point
+    # add (2n choose 2) lines between 2n points
+    # add ((2n choose 2) permute 3) lines such that the length no of 2 lines sums to the length of the 3rd line
 
     # look for a solution with 2*N points (maximum number per pigeonhole principle)
 
     # add 2N unique points to the model
-    points = []
     for i in range(2 * N):
         x_i = Int("x_{0}".format(i))
         y_i = Int("y_{0}".format(i))
         tup = (x_i, y_i)
-        points.append(tup)
         # assert that every point is in the grid
         solver.add(And(x_i >= 0, x_i < N))
         solver.add(And(y_i >= 0, y_i < N))
@@ -184,22 +183,112 @@ def three_solve(N):
                 distinct_points_assertations.append(Or(x_i != x_j, y_i != y_j))
         solver.add(And(distinct_points_assertations))
 
-    # add (2*N choose 2) lines to the model (between every combination of points)
+    # create (2*N choose 2) lines between the 2n points
+    points_2combinations = []
     for i in range((2 * N) - 1):
         for j in range(i + 1, (2 * N)):
-            # y = mx + b
-            # b = y - mx
-            x_i = Int("x_{0}".format(i))
-            y_i = Int("y_{0}".format(i))
-            x_j = Int("x_{0}".format(j))
-            y_j = Int("y_{0}".format(j))
-            m_ij = Int("m_{0}_{1}".format(i, j))
-            b_ij = Int("b_{0}_{1}".format(i, j))
-            solver.add(m_ij == ((y_j - y_i) / (x_j - x_i)))
-            solver.add(b_ij == (y_i - (m_ij * x_i)))
+            # tuple represents the lines between two points. (e.g. (p1, p2) means the line between p1 and p2)
+            # i, j represent the indices of the points created above (e.g., point i == (x_i, y_i))
+            tup = (i, j)
+            points_2combinations.append(tup)
 
-    # assert that the line formed by every point is unique
+    print(points_2combinations)
+
+    # create ((2*N choose 2) choose 3) combinations of lines from the set created above
+    lines_3combinations = []
+    for i in range(0, len(points_2combinations) - 2):
+        for j in range(i + 1, len(points_2combinations) - 1):
+            for k in range(j + 1, len(points_2combinations)):
+                # tuple represents the indices of the lines between any two points.
+                # (e.g. (i,j,k) represents the ith, jth, kth lines from the set created above)
+                tup = (i, j, k)
+                lines_3combinations.append(tup)
+
+    print(lines_3combinations)
+
+    # for each set of 3 combinations of lines, create 3 assertions that the lengths of two lines do not sum to the length of the third
+    for triplet_index in range(len(lines_3combinations)):
+        triplet = lines_3combinations[triplet_index]
+        print("debug line index", triplet[0], triplet[1], triplet[2])
+        # first line in triplet of lines
+        l1 = triplet[0]
+        l1_p1 = points_2combinations[l1][0]  # point (x_{l1_p1}, y_{l1_p1})
+        l1_p2 = points_2combinations[l1][1]  # point (x_{l1_p2}, y_{l1_p2})
+        # second line
+        l2 = triplet[1]
+        l2_p1 = points_2combinations[l2][0]
+        l2_p2 = points_2combinations[l2][1]
+        # third line
+        l3 = triplet[2]
+        l3_p1 = points_2combinations[l3][0]
+        l3_p2 = points_2combinations[l3][1]
+
+        # x and y coordinates of points in l1
+        # point (x_{l1_p1}, y_{l1_p1})
+        l1_p1_x = Int("x_{0}".format(l1_p1))
+        l1_p1_y = Int("y_{0}".format(l1_p1))
+        # point (x_{l1_p2}, y_{l1_p2})
+        l1_p2_x = Int("x_{0}".format(l1_p2))
+        l1_p2_y = Int("y_{0}".format(l1_p2))
+
+        # x and y coordinates of points in l2
+        # point (x_{l1_p1}, y_{l1_p1})
+        l2_p1_x = Int("x_{0}".format(l2_p1))
+        l2_p1_y = Int("y_{0}".format(l2_p1))
+        # point (x_{l1_p2}, y_{l1_p2})
+        l2_p2_x = Int("x_{0}".format(l2_p2))
+        l2_p2_y = Int("y_{0}".format(l2_p2))
+
+        # x and y coordinates of points in l3
+        # point (x_{l1_p1}, y_{l1_p1})
+        l3_p1_x = Int("x_{0}".format(l3_p1))
+        l3_p1_y = Int("y_{0}".format(l3_p1))
+        # point (x_{l1_p2}, y_{l1_p2})
+        l3_p2_x = Int("x_{0}".format(l3_p2))
+        l3_p2_y = Int("y_{0}".format(l3_p2))
+
+        """
+        print("indices: -------------")
+        print(l1_p1, l1_p2)
+        print(l2_p1, l2_p2)
+        print(l3_p1, l3_p2)
+        print("points: --------------")
+        print(l1_p1_x, l1_p1_y)
+        print(l1_p2_x, l1_p2_y)
+        print(l2_p1_x, l2_p1_y)
+        print(l2_p2_x, l2_p2_y)
+        print(l3_p1_x, l3_p1_y)
+        print(l3_p2_x, l3_p2_y)
+        print("----------------------")
+        """
+
+        # add distances between 2 points [(x_a, y_a), (x_b, y_b)]
+        # given by sqrt((x_b - x_a)^2, (y_b - y_a)^2)
+        len_l1 = Real("trip{0}_len1_p{1}_p{2}".format(triplet_index, l1_p1, l1_p2))
+        len_l2 = Real("trip{0}_len2_p{1}_p{2}".format(triplet_index, l2_p1, l2_p2))
+        len_l3 = Real("trip{0}_len3_p{1}_p{2}".format(triplet_index, l3_p1, l3_p2))
+        solver.add(
+            len_l1 == Sqrt(((l1_p2_x - l1_p1_x) ** 2) + ((l1_p2_y - l1_p1_y) ** 2))
+        )
+        solver.add(
+            len_l2 == Sqrt(((l2_p2_x - l2_p1_x) ** 2) + ((l2_p2_y - l2_p1_y) ** 2))
+        )
+        solver.add(
+            len_l3 == Sqrt(((l3_p2_x - l3_p1_x) ** 2) + ((l3_p2_y - l3_p1_y) ** 2))
+        )
+
+        # add assertations for this triplet of lines such that
+        # for any 2 lines, the sum of their lengths do not equal the length of the 3rd line
+        solver.add(len_l1 + len_l2 != len_l3)
+        # solver.add(len_l2 + len_l3 != len_l1)
+        # solver.add(len_l3 + len_l1 != len_l2)
+
+    print(len(points_2combinations), len(lines_3combinations))  # sanity check
+
+    # create ((2*N choose 2) permute 3) constraints that say no two lines can equal the sum of the third
+
     """
+    # assert that the line formed by every point is unique
     In z3, dividing by 0 returns 0. This is a big problem because a 0 slope is indistinguishable from an infinite slope. 
 
     Therefore, when comparing slopes for uniqueness, we must split this into multiple conditions, and we want ANY of these three to pass:
@@ -211,11 +300,9 @@ def three_solve(N):
             i.e., m_ij != m_uv || b_ij != b_uv
     3. This slope is real, and the other slope is infinite (x_i != x_j and x_u == x_v) or (x_i == x_j and x_u != x_v):
         - the slopes are unique by definition. No additional work to be done
-    """
+
     distinct_lines_assertations = []
-    distinct_lines_assertations.append(create_real_assertions(N))
-    distinct_lines_assertations.append(create_one_real_one_inf_assertions(N))
-    """
+    
     for i in range((2 * N) - 1):
         for j in range(i + 1, (2 * N)):
             any_of_three_cases_assertations = []
@@ -258,9 +345,9 @@ def three_solve(N):
                         )
 
             distinct_lines_assertations.append(Or(any_of_three_cases_assertations))
-    """
 
-    solver.add(Or(distinct_lines_assertations))
+    solver.add(And(distinct_lines_assertations))
+    """
 
     # test
     print("----------------------------------------")
